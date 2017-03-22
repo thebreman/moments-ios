cad4578c9bc252ecea41ac42fc6ca209fc5471ce
Andrew Ferrarone <andrewferrarone@Andrews-MacBook-Pro.local>
2017-03-22 01:47:49 -0700

    Creates tab controller navigation with 2 tabs

---
+++ MomentsInTime/MITTabBar.swift
@@ -0,0 +1,46 @@
+//
+//  MITTabBar.swift
+//  MomentsInTime
+//
+//  Created by Andrew Ferrarone on 3/22/17.
+//  Copyright © 2017 Tikkun Olam. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+@IBDesignable
+class MITTabBar: UITabBar
+{
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.23)
+        return view
+    }()
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    /**
+     * add a separator line in the middle of the tab bar, separating the 2 tabs
+     */
+    private func setup()
+    {
+        self.addSubview(self.separatorView)
+        
+        self.separatorView.autoCenterInSuperview()
+        self.separatorView.autoSetDimension(ALDimension.width, toSize: 1)
+        self.separatorView.autoPinEdge(toSuperviewEdge: ALEdge.top, withInset: 8)
+        self.separatorView.autoPinEdge(toSuperviewEdge: ALEdge.bottom, withInset: 8)
+    }
+}
