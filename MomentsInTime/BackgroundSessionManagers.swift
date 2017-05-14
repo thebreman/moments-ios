8707d6b97e1ad7002190d0b9d519f5b84fc1a298
Andrew Ferrarone <andrewferrarone@Andrews-MacBook-Pro.local>
2017-05-13 20:24:50 -0700

    Implements persistent moments for all BackgroundManagers

--- MomentsInTime/BackgroundSessionManagers.swift
+++ MomentsInTime/BackgroundSessionManagers.swift
@@ -7,6 +7,16 @@
 //
 
 import Alamofire
+import RealmSwift
+
+/***** DO NOT CHANGE *****/
+private let USER_DEFAULT_SUITE_UPLOAD = "CurrentUserUpload"
+private let USER_DEFAULT_SUITE_COMPLETE_UPLOAD = "CurrentUserUploadComplete"
+private let USER_DEFAULT_SUITE_UPLOAD_METADATA = "CurrentUserUploadMetadata"
+private let DEFAULTS_KEY_MOMENT_UPLOAD = "momentIDKey"
+private let DEFAULTS_KEY_MOMENT_UPLOAD_COMPLETE = "momentIDKey"
+private let DEFAULTS_KEY_MOMENT_UPLOAD_METADATA = "momentIDKey"
+/***** DO NOT CHANGE *****/
 
 /**
  * Create a session manager with background configuration.
@@ -16,6 +26,8 @@ import Alamofire
 
 class BackgroundUploadSessionManager: Alamofire.SessionManager
 {
+    private static var userDefaults = UserDefaults(suiteName: USER_DEFAULT_SUITE_UPLOAD)! // do not modify name
+    
     //guaranteed to be lazily initialized only once, even when accessed across multiple threads simultaneously:
     static let shared: BackgroundUploadSessionManager = {
         let configuration = URLSessionConfiguration.background(withIdentifier: "com.momentsintime.app.background")
@@ -26,11 +38,30 @@ class BackgroundUploadSessionManager: Alamofire.SessionManager
     }()
     
     var systemCompletionHandler: (() -> Void)?
-    var moment: Moment?
+    
+    var moment: Moment? {
+        get {
+            if let momentPrimaryKey = BackgroundUploadSessionManager.userDefaults.string(forKey: DEFAULTS_KEY_MOMENT_UPLOAD),
+                let realm = try? Realm() {
+                return realm.object(ofType: Moment.self, forPrimaryKey: momentPrimaryKey)
+            }
+            return nil
+        }
+        set {
+            if newValue == nil {
+                BackgroundUploadSessionManager.userDefaults.removeObject(forKey: DEFAULTS_KEY_MOMENT_UPLOAD)
+            }
+            else {
+                BackgroundUploadSessionManager.userDefaults.set(newValue!.momentID, forKey: DEFAULTS_KEY_MOMENT_UPLOAD)
+            }
+        }
+    }
 }
 
 class BackgroundUploadCompleteSessionManager: Alamofire.SessionManager
 {
+    private static var userDefaults = UserDefaults(suiteName: USER_DEFAULT_SUITE_COMPLETE_UPLOAD)! // do not modify name
+    
     static let shared: BackgroundUploadCompleteSessionManager = {
         let configuration = URLSessionConfiguration.background(withIdentifier: "com.momentsintime.app.backgroundcomplete")
         configuration.timeoutIntervalForRequest = 3600
@@ -39,13 +70,33 @@ class BackgroundUploadCompleteSessionManager: Alamofire.SessionManager
         return sessionManager
     }()
     
-    var systemCompletionHandler: (() -> Void)?
-    var moment: Moment?
     var completeURI: String?
+    
+    var systemCompletionHandler: (() -> Void)?
+    
+    var moment: Moment? {
+        get {
+            if let momentPrimaryKey = BackgroundUploadCompleteSessionManager.userDefaults.string(forKey: DEFAULTS_KEY_MOMENT_UPLOAD_COMPLETE),
+                let realm = try? Realm() {
+                return realm.object(ofType: Moment.self, forPrimaryKey: momentPrimaryKey)
+            }
+            return nil
+        }
+        set {
+            if newValue == nil {
+                BackgroundUploadCompleteSessionManager.userDefaults.removeObject(forKey: DEFAULTS_KEY_MOMENT_UPLOAD_COMPLETE)
+            }
+            else {
+                BackgroundUploadCompleteSessionManager.userDefaults.set(newValue!.momentID, forKey: DEFAULTS_KEY_MOMENT_UPLOAD_COMPLETE)
+            }
+        }
+    }
 }
 
 class BackgroundUploadVideoMetadataSessionManager: Alamofire.SessionManager
 {
+    private static var userDefaults = UserDefaults(suiteName: USER_DEFAULT_SUITE_UPLOAD_METADATA)! // do not modify name
+    
     static let shared: BackgroundUploadVideoMetadataSessionManager = {
         let configuration = URLSessionConfiguration.background(withIdentifier: "com.momentsintime.app.backgroundMetadata")
         configuration.timeoutIntervalForRequest = 3600
@@ -55,6 +106,23 @@ class BackgroundUploadVideoMetadataSessionManager: Alamofire.SessionManager
     }()
     
     var systemCompletionHandler: (() -> Void)?
-    var moment: Moment?
+    
+    var moment: Moment? {
+        get {
+            if let momentPrimaryKey = BackgroundUploadVideoMetadataSessionManager.userDefaults.string(forKey: DEFAULTS_KEY_MOMENT_UPLOAD_METADATA),
+                let realm = try? Realm() {
+                return realm.object(ofType: Moment.self, forPrimaryKey: momentPrimaryKey)
+            }
+            return nil
+        }
+        set {
+            if newValue == nil {
+                BackgroundUploadVideoMetadataSessionManager.userDefaults.removeObject(forKey: DEFAULTS_KEY_MOMENT_UPLOAD_METADATA)
+            }
+            else {
+                BackgroundUploadVideoMetadataSessionManager.userDefaults.set(newValue!.momentID, forKey: DEFAULTS_KEY_MOMENT_UPLOAD_METADATA)
+            }
+        }
+    }
 }
 
