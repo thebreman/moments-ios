c2b8c4e8cd80f7a5f62d3d77ceb3b5e45ae26907
Andrew Ferrarone <andrewferrarone@Andrews-MacBook-Pro.local>
2017-05-19 22:16:43 -0700

    Organizes file system

---
+++ MomentsInTime/HowAreWeDoingAlertView.swift
@@ -0,0 +1,14 @@
+//
+//  HowAreWeDoingAlertView.swift
+//  MomentsInTime
+//
+//  Created by Andrew Ferrarone on 5/19/17.
+//  Copyright © 2017 Tikkun Olam. All rights reserved.
+//
+
+import UIKit
+
+class HowAreWeDoingAlertView: NSObject
+{
+    //
+}
