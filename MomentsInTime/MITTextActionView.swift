b14eea7598c3f2487f9d1c31555366fa0cda2906
Andrew Ferrarone <apferrarone@gmail.com>
2017-06-11 23:13:28 -0400

    Updates Welcome Header View text

--- MomentsInTime/MITTextActionView.swift
+++ MomentsInTime/MITTextActionView.swift
@@ -19,8 +19,8 @@ private let COPY_TITLE_BUTTON_EMPTY_STATE = "Let's make a moment"
 private let COPY_LET_MESSAGE_COMMUNITY_EMPTY_STATE = "Uh Oh. There are no moments yet."
 
 private let COPY_TITLE_WELCOME = "Welcome to Moments in Time!"
-private let COPY_MESSAGE_WELCOME = "We believe that these stories will inspire, educate, and encourage others to capture their own \"Moments In Time,\" connecting one generation to another while preserving amazing stories."
-private let COPY_TITLE_BUTTON_WELCOME = "Let's go!"
+private let COPY_MESSAGE_WELCOME = "Watch Moments from the community and get inspired to share your own Moments. Scroll down to start."
+private let COPY_TITLE_BUTTON_WELCOME = "Ok, Let's go!"
 
 private let COPY_TITLE_SHARE_LIVE_MOMENT = "You made a Moment!"
 private let COPY_MESSAGE_SHARE_LIVE_MOMENT = "People are going to love your post. Want to share your creation?"
